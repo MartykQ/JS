6c9537c28ae7e7f73f9326e6b72c39c6307f143e
MartykQ <fmartyka16@gmail.com>
2018-11-10 15:25:15 +0100

    a
    a

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
 
 gulp.task('hello', function() {
     console.log('Hello world');
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
+    });
 });
